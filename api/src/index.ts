e89914c91ca6f0753c211435fbab6a288cebdc9b
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-06-10 13:33:59 +0530

    feat: new api tested and working

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,7 +5,19 @@ const app = new Hono<{ Bindings: Env }>();
 app.get("/api/health", (c) => c.json({ status: "ok" }));
 
 app.post("/api/surveys", async (c) => {
-  return c.json({ message: "working" });
+  await c.env.DB.prepare(
+    "INSERT INTO surveys (title, description) VALUES (?, ?)",
+  )
+    .bind("sdasdasda", "description")
+    .run();
+
+  return c.json({ success: true})
+});
+
+app.get("api/surveys", async (c) => {
+  const result = await c.env.DB.prepare("SELECT * FROM surveys").all();
+
+  return c.json(result.results);
 });
 
 export default app;
