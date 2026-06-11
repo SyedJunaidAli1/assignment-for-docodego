ef7eea202b698ca4a238f789d8e21037fa5e504f
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-06-11 13:06:34 +0530

    feat: tailwind installed in web

--- web/src/routes/signin.tsx
+++ web/src/routes/signin.tsx
@@ -1,9 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/signin')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/signin")({
+  component: SignInPage,
+});
 
-function RouteComponent() {
-  return <div>Hello "/signin"!</div>
+function SignInPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Sign In</h1>
+    </div>
+  );
 }
